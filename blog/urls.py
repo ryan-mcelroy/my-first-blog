ea119b4323e8df94c47fe9ab220b4b295e419097
ryan <ryan@gaiagps.com>
2020-11-03 09:43:24 -0500

    fix typo

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path    # import the 'path' function from django
 from . import views             # import all the views from 'blog' app (we don't have any yet)
 
 urlpatterns = [                 # assigning a view called 'post_list' to root URL
-    path('', view.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
 ]
 
