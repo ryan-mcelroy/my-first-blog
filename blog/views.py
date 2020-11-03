37db3104c96263e00ae66952c42f276b30eaec12
ryan <ryan@gaiagps.com>
2020-11-03 09:58:02 -0500

    add view to blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# function that takes a request and returns a value received from 'render'
+# 'render' will put together the template
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
