d617e499eff5704cf04fac7d7a98b2e441de1b9f
ryan <ryan@gaiagps.com>
2020-11-04 12:06:01 -0500

    save button creates new post + redirects

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,10 @@
+from django.shortcuts import redirect # allows us to direct to new url
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404  # allow for non-existant objects
 from django.utils import timezone               # let us sort by time
 from .models import Post        # lets us make things dynamic - connect model to template through view
 from .forms import PostForm     # import the new PostForm model
 
-# Create your views here.
 
 # function that takes a request and returns a value received from 'render'
 # 'render' will put together the template
@@ -21,5 +21,14 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST": # if there's data to submit, construct PostForm with the data
+        form = PostForm(request.POST)
+        if form.is_valid():      # check form data has been entered correctly so we can save it
+            post = form.save(commit=False) # soft-save, allows us to add other details below
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save() # save the blog post
+            return redirect('post_detail', pk=post.pk) # direct to the post_detail view if new blogpost created
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
