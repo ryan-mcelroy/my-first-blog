add2bf7ad35b16455c3d4dfc4ba680b65c2d4080
ryan <ryan@gaiagps.com>
2020-11-04 13:30:18 -0500

    add edit post button and feature

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [                 # assigning a view called 'post_list' to root UR
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'), 
 ]
 
